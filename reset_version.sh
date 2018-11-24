adbac26edf6eb725a7afc21119fee71968d43141
em <eugen.mayer@kontextwork.de>
2018-11-24 09:42:22 +0100

    add reset versions script

---
+++ reset_version.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# flush local tags for a clean state with fetch
+git tag -d $(git tag -l)
+git fetch
+# delete remote tags
+git push origin --delete $(git tag -l)
+# now also delete locally
+git tag -d $(git tag -l)
+
+git checkout version
+echo "0.0.0" > release-version
+echo "0.0.0" > candidate-version
+echo "0.0.0" > staging-version
+git add *-version
+git commit -am "reset versions"
+git push
